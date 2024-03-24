4ed1099fd6ec9220e7a291906690880c5053b7f8
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-24 12:00:54 +0100

    two sum with more test

--- test/TwoSumTest.java
+++ test/TwoSumTest.java
@@ -1,15 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TwoTest {
+public class TwoSumTest {
+    private TwoSum twoSum;
+    @BeforeEach
+    public void intializeTwoSum(){
+        twoSum = new TwoSum();
+    }
 
     @Test
-    public void testTwoSum(){
-        int[] array = {2,7,11,15};
+    public void testThatTheTwoIndexReturnNine(){
+        int[] nums = {2,7,11,15};
         int[] expected = {0,1};
-        TwoSum twoSum = new TwoSum();
-        assertArrayEquals(expected, twoSum.getTwoSum(array, 9));
+        assertArrayEquals(expected, twoSum.getTwoSum(nums, 9));
+    }
+
+    @Test
+    public void testThatTheTwoIndexReturnSix(){
+        int[] nums = {3,2,4};
+        int[] expected = {1,2};
+        assertArrayEquals(expected, twoSum.getTwoSum(nums, 6));
+    }
+
+    @Test
+    public void testThatTheTwoIndexWillAlsoReturnSix(){
+        int[] nums = {3,3};
+        int[] expected = {0,1};
+        assertArrayEquals(expected, twoSum.getTwoSum(nums, 6));
     }
 }
